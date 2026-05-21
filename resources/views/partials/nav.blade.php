d093902538948d60fef3edde8e781654c0c25e19
Zornitsa Marinova <zornitsa.marinova93@gmail.com>
2026-05-21 12:10:57 +0300

    changes

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -102,6 +102,7 @@
 
   <div class="nav-drawer-footer">
     <a href="tel:+33782085117" class="nav-drawer-phone">
+      <img src="{{ asset('images/thierry.jpeg') }}" alt="Thierry Sudol" class="nav-person-avatar">
       <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07A19.5 19.5 0 013.07 9.8a19.79 19.79 0 01-3.07-8.67A2 2 0 012 2h3a2 2 0 012 1.72c.127.96.361 1.903.7 2.81a2 2 0 01-.45 2.11L6.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0122 16.92z"/>
       </svg>
